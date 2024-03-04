ebdeb264e928550d0c13b1cd01bca03a688dfaa7
Josh Wood <joshua.r.wood@nasa.gov>
2024-03-03 21:07:48 -0600

    working on version with open source tiles

--- convert.py
+++ convert.py
@@ -10,15 +10,18 @@ output = [
 ]
 
 fin = open(course_gpx)
+fout = open(f"routes/{route_name}.js", 'w')
+fout.write(f"var {route_name}_points = [\n")
 for line in fin:
    if "trkpt lat" in line:
        _, lat, lon = line.split()
-       output[0] += lat[5:-1] + ", "
-       output[1] += lon[5:-2] + ", "
+       fout.write(f"  [{lat[5:-1]}, {lon[5:-2]}],\n")
+fout.write("];\n")
+fout.close()
 fin.close()
 
-fout = open(f"routes/{route_name}.js", 'w')
-fout.write(output[0][:-2] + "];\n")
-fout.write(output[1][:-2] + "];\n")
-fout.close()
+#fout = open(f"routes/{route_name}.js", 'w')
+#fout.write(output[0][:-2] + "];\n")
+#fout.write(output[1][:-2] + "];\n")
+#fout.close()
 
